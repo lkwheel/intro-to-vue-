d35945693b01799d81bc4c5c5d809738c3796916
lkwheel <lkriswheeler@gmail.com>
2022-11-12 16:38:36 -0500

    removed BaseImage component in favor of using webpack url-loader

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,14 @@
       <div class="product-container">
         <div
           class="product-image"
-          :class="{ 'out-of-stock-img': !(inventory > 0) }"
+          :class="{ 'out-of-stock-img': inStock <= 0 }"
         >
-          <BaseImage :src="src" :alt="alt" />
+          <img :src="src" :alt="alt" />
         </div>
         <div class="product-info">
           <h1>{{ title }}</h1>
-          <p v-if="inventory > 10">In stock</p>
-          <p v-else-if="inventory <= 10 && inventory > 0">
+          <p v-if="inStock > 10">In stock</p>
+          <p v-else-if="inStock <= 10 && inStock">
             Almost out of stock
           </p>
           <p v-else>Out of stock</p>
@@ -25,16 +25,16 @@
             </li>
           </ul>
           <div
-            v-for="variant in variants"
+            v-for="(variant, index) in variants"
             :key="variant.id"
             class="color-circle"
             :class="[variant.image === src ? activeClass : '']"
             :style="{ backgroundColor: variant.color }"
-            @mouseover="updateImage(variant)"
+            @mouseover="updateVariant(index)"
           ></div>
           <button
             class="button"
-            :class="{ disabledButton: !(inventory > 0) }"
+            :class="{ disabledButton: !inStock }"
             :disabled="!(inventory > 0)"
             @click="addToCart"
           >
@@ -47,39 +47,51 @@
 </template>
 
 <script>
-const greenSocks = "socks_green.jpg";
-const greenSocksAlt = "Green Socks";
-const blueSocks = "socks_blue.jpg";
-const blueSocksAlt = "Blue Socks";
-import BaseImage from "./components/BaseImage";
+import blueSocks from "@/assets/images/socks_blue.jpg";
+import greenSocks from "@/assets/images/socks_green.jpg";
 export default {
   name: "App",
-  components: {
-    BaseImage,
-  },
+  components: {},
   data: () => {
     return {
-      activeClass: 'active-circle',
+      activeClass: "active-circle",
       cart: 0,
       product: "Socks",
       brand: "Vue Mastery",
       inventory: 10,
       details: ["50% cotton", "30% wool", "20% ployester"],
-      src: blueSocks,
-      alt: blueSocksAlt,
+      selectedVariant: 0,
       variants: [
-        { id: 2234, color: "green", image: greenSocks, alt: greenSocksAlt, quantity: 50 },
-        { id: 2235, color: "blue", image: blueSocks, alt: blueSocksAlt, quantity: 0 },
+        {
+          id: 2234,
+          color: "green",
+          image: greenSocks,
+          alt: "Green Socks",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: blueSocks,
+          alt: "Blue Socks",
+          quantity: 0,
+        },
       ],
     };
   },
   computed: {
-    // inStock() {
-    //   return this.inventory > 0;
-    // }
     title() {
       return this.brand + " " + this.product;
     },
+    src() {
+      return this.variants[this.selectedVariant].image;
+    },
+    alt() {
+      return this.variants[this.selectedVariant].alt;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    }
   },
   methods: {
     // Note: don't use arrow functions on instance properties
@@ -87,9 +99,8 @@ export default {
     addToCart() {
       this.cart = ++this.cart;
     },
-    updateImage(variant) {
-      this.src = variant.image;
-      this.alt = variant.alt;
+    updateVariant(index) {
+      this.selectedVariant = index;
     },
   },
 };
